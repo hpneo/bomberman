772404914f5bfa741ebaf12570c9e679f18cd38e
swbvelasquez <swbvelasquez@gmail.com>
2015-10-24 18:10:30 +0200

    Update main.js
    Cambio de Menu por GameOver

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,6 @@ var game = new Phaser.Game(15 * 32, 9 * 32, Phaser.AUTO);
 game.state.add('Boot', Boot);
 game.state.add('Preloader', Preloader);
 game.state.add('Intro', Intro);
-game.state.add('Menu', Menu);
+game.state.add('GameOver', GameOver);
 game.state.add('Game', Game);
 game.state.start('Boot');
